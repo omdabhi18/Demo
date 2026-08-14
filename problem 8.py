4dab2f410662e1001fce0eb39c8c991522ed374f
chavdaparth034 <chavdaaparthh0111@gmail.com>
2026-08-14 16:24:58 +0530

    aa riyo code taro
    have kai na magto

---
+++ problem 8.py
@@ -0,0 +1,11 @@
+n = int(input("enter a number :"))
+# i = 1
+# while(i<11):
+#     t = i*n
+#     print(f"{n} X {i} =",t)
+#     i +=1 
+i = 0
+sum = 0
+while(i<=n):
+    sum += i
+print("sum of all natural nuber is: ",sum)
